46416b85d2bf84302a0555c8309ac3416159b092
Jakub Mijal <237063985+jem261@users.noreply.github.com>
2026-09-01 19:53:10 +0100

    updated parse function

--- solutions/2015/day02.py
+++ solutions/2015/day02.py
@@ -3,11 +3,13 @@ def parse():   # parsing is turning raw text into structured data
    with open("inputs/2015/day02.txt") as f:
       data = f.read().splitlines()
 
-   list = []
-   for line in data:
-      list.append(line.split("x"))
+   boxes = []
 
-   for p in list:
-      p = int(p) 
+   for string in data:
+      pieces = string.split("x")
+      numbers = [int(p) for p in pieces]
+      boxes.append(numbers)
+
+   return boxes
     
 

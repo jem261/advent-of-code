98e04ab0928d3516027593629387c2be86165426
Jakub Mijal <237063985+jem261@users.noreply.github.com>
2026-09-03 17:42:33 +0100

    completed day two solution

--- solutions/2015/day02.py
+++ solutions/2015/day02.py
@@ -1,15 +1,22 @@
-def parse():   # parsing is turning raw text into structured data
-   
-   with open("inputs/2015/day02.txt") as f:
-      data = f.read().splitlines()
+def parse(input):
+    data = input.splitlines()
+    boxes = []
+    for string in data:
+       pieces = string.split("x")
+       numbers = [int(p) for p in pieces]
+       boxes.append(numbers)
+    return boxes
 
-   boxes = []
+with open("inputs/2015/day02.txt") as f:
+    s = f.read()
 
-   for string in data:
-      pieces = string.split("x")
-      numbers = [int(p) for p in pieces]
-      boxes.append(numbers)
+def paper_calc(list):
+    total = 0
+    for l, w, h in list:
+        sides = [l*w, w*h, l*h]
+        total += (2 * sum(sides)) + min(sides)
+    return total
 
-   return boxes
-    
+amount = paper_calc(parse(s))
 
+print(f"The elves require {amount}m^2 of wrapping paper!")

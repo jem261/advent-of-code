d3c0de05dadcd9ed1f59680b9b8bfd68e0dbf180
Jakub Mijal <237063985+jem261@users.noreply.github.com>
2026-09-06 21:36:35 +0100

    completed day 3 part two

--- solutions/2015/day03.py
+++ solutions/2015/day03.py
@@ -11,6 +11,28 @@ def map_reader(map):
 with open("inputs/2015/day03.txt") as f:
     s = f.read()
 
-houses = map_reader(s)
+houses1 = map_reader(s)
 
-print(f"{houses} houses recieved at least 1 present!")
+print(f"{houses1} houses recieved at least 1 present!")
+
+def alternate_turns(map):
+    directions = {"^": (0,1), "v": (0, -1), ">": (1,0), "<": (-1,0)}
+    visited_santa = {(0, 0)}
+    visited_robosanta = {(0, 0)}
+    x1, y1 = 0, 0
+    x2, y2 = 0, 0
+    santa_map = map[::2]
+    robosanta_map = map[1::2]
+    for char in santa_map:
+        dx1, dy1 = directions[char]
+        x1, y1 = x1+dx1, y1+dy1
+        visited_santa.add((x1,y1))
+    for char in robosanta_map:
+        dx2, dy2 = directions[char]
+        x2, y2 = x2+dx2, y2+dy2
+        visited_robosanta.add((x2,y2))
+    return len(visited_robosanta | visited_santa)
+
+houses2 = alternate_turns(s)
+
+print(f"{houses2} houses recieved at least 1 present!")

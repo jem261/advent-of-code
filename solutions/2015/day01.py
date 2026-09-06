05200ae58c75334a27e780cdfac7ac3d93e56162
Jakub Mijal <237063985+jem261@users.noreply.github.com>
2026-09-06 19:48:36 +0100

    completed day 1 part two

--- solutions/2015/day01.py
+++ solutions/2015/day01.py
@@ -6,14 +6,26 @@ def char_counter(s, char):
     return count
 
 with open("inputs/2015/day01.txt") as f:
-   s = f.read()
+   s = f.read().strip()
 
 char1 = "("
 char2 = ")"
 
-up = (char_counter(s, char1))
-down = (char_counter(s, char2))
+up = char_counter(s, char1)
+down = char_counter(s, char2)
 
-floor = up - down
+floor_s = up - down
+floor_d = 0
+position = 0
 
-print(f"Santa is on floor {floor}!")
+for ch in s:
+    if floor_d != -1:
+        position += 1
+        if ch == "(":
+            floor_d += 1
+        elif ch == ")":
+            floor_d -= 1
+    elif floor_d == -1:
+        break
+
+print(f"Santa is on floor {floor_s}! He reached the basement on position number {position}")

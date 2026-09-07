c21e26f0234f11bae135c739c4649a0ba3f015f5
Jakub Mijal <237063985+jem261@users.noreply.github.com>
2026-09-07 22:49:49 +0100

    completed day 6 part two

--- solutions/2015/day06.py
+++ solutions/2015/day06.py
@@ -4,7 +4,7 @@ with open("inputs/2015/day06.txt") as f:
     strings = f.read().splitlines()
 
 lights_on = 0
-grid = [[False] * 1000 for _ in range(1000)]
+grid1 = [[False] * 1000 for _ in range(1000)]
 
 for string in strings:
     data = re.findall(r"\d+", string)
@@ -12,18 +12,44 @@ for string in strings:
     if string.startswith("toggle"):
         for i in range(nums[1], nums[3]+1):
             for j in range(nums[0], nums[2]+1):
-                grid[i][j] = not grid[i][j]
+                grid1[i][j] = not grid1[i][j]
     elif string.startswith("turn on"):
         for i in range(nums[1], nums[3]+1):
-                    for j in range(nums[0], nums[2]+1):
-                        grid[i][j] = True
+            for j in range(nums[0], nums[2]+1):
+                grid1[i][j] = True
     elif string.startswith("turn off"):
         for i in range(nums[1], nums[3]+1):
-                    for j in range(nums[0], nums[2]+1):
-                        grid[i][j] = False
+            for j in range(nums[0], nums[2]+1):
+                grid1[i][j] = False
     
 for i in range(1000):
-    lights_on += sum(grid[i])
+    lights_on += sum(grid1[i])
 
 print(f"There are {lights_on} lights on!")
 
+brightness = 0
+grid2 = [[0] * 1000 for _ in range(1000)]
+
+for string in strings:
+    data = re.findall(r"\d+", string)
+    nums = [int(p) for p in data]
+    if string.startswith("toggle"):
+        for i in range(nums[1], nums[3]+1):
+            for j in range(nums[0], nums[2]+1):
+                grid2[i][j] += 2
+    elif string.startswith("turn on"):
+        for i in range(nums[1], nums[3]+1):
+            for j in range(nums[0], nums[2]+1):
+                grid2[i][j] += 1
+    elif string.startswith("turn off"):
+        for i in range(nums[1], nums[3]+1):
+            for j in range(nums[0], nums[2]+1):
+                if grid2[i][j] == 0:
+                    pass
+                elif grid2[i][j] > 0:
+                    grid2[i][j] -= 1
+
+for i in range(1000):
+    brightness += sum(grid2[i])
+
+print(f"The total brightness is {brightness}!")

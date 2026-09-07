98e2f6a6bc11285256e12606d782617cbd238c2c
Jakub Mijal <237063985+jem261@users.noreply.github.com>
2026-09-07 22:20:28 +0100

    completed day 4 part two

--- solutions/2015/day04.py
+++ solutions/2015/day04.py
@@ -3,12 +3,22 @@ import hashlib
 with open("inputs/2015/day04.txt") as f:
     s = f.read()
 
-ans = ""
+ans1 = ""
 n = 1
 
-while ans[:5] != "00000":
+while ans1[:5] != "00000":
     text = s + str(n)
-    ans = hashlib.md5(text.encode()).hexdigest()
+    ans1 = hashlib.md5(text.encode()).hexdigest()
     n += 1
 
 print(n-1)
+
+ans2 = ""
+m = 0
+
+while ans2[:6] != "000000":
+    text = s + str(m)
+    ans2 = hashlib.md5(text.encode()).hexdigest()
+    m += 1
+
+print(m-1)
